aac41a37bb2fafd3e24d9a5dc2dbc0c65d7fb2f3
signin404 <145902543+signin404@users.noreply.github.com>
2025-07-29 08:32:38 +0800

    Add files via upload

---
+++ DwmEnableMMCSS.ps1
@@ -0,0 +1,10 @@
+﻿$signature = @'
+[DllImport("dwmapi.dll", SetLastError=true)]
+public static extern int DwmEnableMMCSS(bool enable);
+'@
+$dwmapi = Add-Type -MemberDefinition $signature -Name "DwmApi" -Namespace "API" -PassThru
+
+while ($true) {
+    $result = $dwmapi::DwmEnableMMCSS($true)
+    Start-Sleep -Seconds 60
+}
